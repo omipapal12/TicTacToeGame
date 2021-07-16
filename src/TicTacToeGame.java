b0c55250599c88820c9b530091b6c4d8b352a31c
omipapal12 <omipapal12@gmail.com>
2021-07-16 21:09:42 +0530

    Added: Letter X or O

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,14 +1,22 @@
-
+import java.util.Scanner;
 public class TicTacToeGame {
-	public static char[] CreateBoard() {
+	public static char[] createBoard() {
 		char[] board = new char[10];
-		for( int i=1;i<10;i++) {
+		for(int i = 0; i<10; i++) {
 			board[i]=' ';
 		}
 		return board;
 }
+	private static char UserLetter(){
+		Scanner scan = new Scanner(System.in);
+	 	System.out.println("Enter choice X or O");
+	 	char UserLetter = scan.next().charAt(0);
+	 	return UserLetter;
+ }
+
 	public static void main(String[] args) {
 		System.out.println("WELCOME TO THE TIC_TAC_TOE_GAME");
-		char[] board=CreateBoard();
-}
+		char[] board=createBoard();
+		UserLetter();
+	}
 }
