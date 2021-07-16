038d729bcf33a5d11728e19819a848ff8ad8a411
omipapal12 <86665077+omipapal12@users.noreply.github.com>
2021-07-16 09:44:30 +0530

    Update TicTacToeGame.java

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,14 +1,14 @@
 
 public class TicTacToeGame {
-	public static char[] CreateBoard() {
+	public static char[] createBoard() {
 		char[] board = new char[10];
-		for( int i=1;i<10;i++) {
+		for(int i = 0; i<10; i++) {
 			board[i]=' ';
 		}
 		return board;
 }
 	public static void main(String[] args) {
 		System.out.println("WELCOME TO THE TIC_TAC_TOE_GAME");
-		char[] board=CreateBoard();
+		char[] board=createBoard();
 }
 }
