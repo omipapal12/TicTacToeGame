132fe3da11518d8108e3cacb50bad83e59ba0282
omipapal12 <omipapal12@gmail.com>
2021-07-16 23:06:48 +0530

    Added: TicTacToe Game Board

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -13,10 +13,20 @@ public class TicTacToeGame {
 	 	char UserLetter = scan.next().charAt(0);
 	 	return UserLetter;
  }
-
+	public static void showBoard(char[] board) { 
+		 System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"); 
+		 System.out.println("    |-----|-----|-----| "); 
+		 System.out.println("    |  "+board[1]+"  |  "+board[2]+"  |  "+board[3]+"  |"); 
+		 System.out.println("    |-----|-----|-----| "); 
+		 System.out.println("    |  "+board[4]+"  |  "+board[5]+"  |  "+board[6]+"  |"); 
+		 System.out.println("    |-----|-----|-----| "); 
+		 System.out.println("    |  "+board[7]+"  |  "+board[8]+"  |  "+board[9]+"  |"); 
+		 System.out.println("    |-----|-----|-----| "); System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");	    
+		 } 
 	public static void main(String[] args) {
 		System.out.println("WELCOME TO THE TIC_TAC_TOE_GAME");
 		char[] board=createBoard();
 		UserLetter();
+		showBoard(board);
 	}
 }
